043994081b74520abd58a6c0898956ced5bf07c2
Derek Chuank <derekchuank@outlook.com>
2022-11-15 13:13:25 +0800

    add support for specifying output to be modified.

--- xwhite.c
+++ xwhite.c
@@ -1,81 +1,110 @@
+#include <X11/extensions/Xrandr.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <X11/extensions/Xrandr.h>
+#include <string.h>
 
-int
-setGamma (double red, double green, double blue)
-{
+int setGamma(char *output_name, double red, double green, double blue) {
   Display *d;
   XRRScreenResources *xsr = NULL;
   XRRCrtcGamma *xcg;
   double gvalue;
   int gammaSize;
+  int found = 0;
 
-  if (!(d = XOpenDisplay (NULL))
-      || !(xsr =
-	   XRRGetScreenResourcesCurrent (d,
-					 RootWindow (d, DefaultScreen (d)))))
-    {
-      printf ("XOpenDisplay or XRRGetScreenResourcesCurrent failed");
+  if (!(d = XOpenDisplay(NULL)) || !(xsr = XRRGetScreenResourcesCurrent(
+                                         d, RootWindow(d, DefaultScreen(d))))) {
+    printf("XOpenDisplay or XRRGetScreenResourcesCurrent failed");
+    return 1;
+  }
+
+  for (int o = 0; o < xsr->noutput; o++) {
+    XRROutputInfo *output_info = XRRGetOutputInfo(d, xsr, xsr->outputs[o]);
+    if (!output_info) {
+      printf("could not get output 0x%lx information\n", xsr->outputs[o]);
       return 1;
     }
 
-  for (int i = 0; i < xsr->ncrtc; i++)
-    {
-      gammaSize = XRRGetCrtcGammaSize (d, xsr->crtcs[i]);
-      if (!(xcg = XRRAllocGamma (gammaSize)))
-	{
-	  printf ("XRRAllocGamma failed");
-	  return 1;
-	}
+    RRCrtc crtc = output_info->crtc;
 
-      for (int j = 0; j < gammaSize; j++)
-	{
-	  gvalue = 65535.0 * j / gammaSize;
-	  xcg->red[j] = gvalue * red;
-	  xcg->green[j] = gvalue * green;
-	  xcg->blue[j] = gvalue * blue;
-	}
+    if (!strcmp(output_name, output_info->name)) {
+      found++;
 
-      XRRSetCrtcGamma (d, xsr->crtcs[i], xcg);
-      XFree (xcg);
+      gammaSize = XRRGetCrtcGammaSize(d, crtc);
+      if (!(xcg = XRRAllocGamma(gammaSize))) {
+        printf("XRRAllocGamma failed");
+        return 1;
+      }
+
+      for (int j = 0; j < gammaSize; j++) {
+        gvalue = 65535.0 * j / gammaSize;
+        xcg->red[j] = gvalue * red;
+        xcg->green[j] = gvalue * green;
+        xcg->blue[j] = gvalue * blue;
+      }
+
+      XRRSetCrtcGamma(d, crtc, xcg);
+      XFree(xcg);
     }
+    XFree(output_info);
+  }
 
-  XFree (xsr), XCloseDisplay (d);
+  if (found == 0) {
+    printf("Output not found: %s.\n", output_name);
+    return 1;
+  }
+
+  XFree(xsr), XCloseDisplay(d);
 
   return 0;
 }
 
-int
-main (int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   double rgb[3];
+  char *output_name;
   int result;
 
-  if (argc != 4)
-    {
-      printf ("Usage: xwhite RED GREEN BLUE\n"
-	      "For example, set color temperture 3000K:\n"
-	      "    xwhite 1.00000000 0.71976951 0.42860152");
-      exit (1);
+  if (argc < 5) {
+    printf(
+        "Usage: xwhite --output <crtc> --rgb <red>:<green>:<blue>\n"
+        "For example, set color temperture 3000K on a monitor named LVDS-1:\n"
+        "    xwhite --output LVDS-1 --rgb "
+        "1.00000000:0.71976951:0.42860152\n");
+    exit(1);
+  }
+
+  for (int i = 1; i < argc; i++) {
+    if (!strcmp("--output", argv[i])) {
+      if (++i >= argc) {
+        printf("%s requires an argument\n", argv[i - 1]);
+        exit(1);
+      }
+      output_name = argv[i];
+
+      continue;
+    }
+    if (!strcmp("--rgb", argv[i])) {
+      if (++i >= argc)
+        printf("%s requires an argument\n", argv[i - 1]);
+      if (sscanf(argv[i], "%lf:%lf:%lf", &rgb[0], &rgb[1], &rgb[2]) != 3) {
+        printf("%s: invalid argument '%s'\n", argv[i - 1], argv[i]);
+        exit(1);
+      }
+      if (rgb[i] < 0 || rgb[i] > 1) {
+        printf("R, G and B parameters must be between 0 and 1.\n");
+        exit(1);
+      }
+      continue;
     }
 
-  for (int i = 0; i < 3; i++)
-    {
-      rgb[i] = strtod (argv[i + 1], NULL);
-      if (rgb[i] < 0 || rgb[i] > 1)
-	{
-	  printf ("R, G and B parameters must be between 0 and 1.\n");
-	  exit (1);
-	}
-    }
+    printf("unrecognized option %s\n", argv[i]);
+    exit(1);
+  }
 
-  result = setGamma (rgb[0], rgb[1], rgb[2]);
-  if (result != 0)
-    {
-      printf ("Set gamma failed.");
-      exit (1);
-    }
+  result = setGamma(output_name, rgb[0], rgb[1], rgb[2]);
+  if (result != 0) {
+    printf("Set gamma failed.\n");
+    exit(1);
+  }
 
   return 0;
 }
